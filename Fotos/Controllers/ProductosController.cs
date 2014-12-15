3a5939c53a504d714dec5c78877bfaefc4e011bc
luisgiltajamar <luis.gil@tajamar365.com>
2014-12-15 12:42:16 +0100

    a

---
+++ Fotos/Controllers/ProductosController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Fotos.Models;
+
+namespace Fotos.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private DemoFicherosEntities db = new DemoFicherosEntities();
+
+        // GET: Productos
+        public ActionResult Index()
+        {
+            return View(db.Productos.ToList());
+        }
+
+        // GET: Productos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productos productos = db.Productos.Find(id);
+            if (productos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productos);
+        }
+
+        // GET: Productos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Productos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Productos productos,HttpPostedFileBase foto)
+        {
+            String[] validos = {"jpg", "gif", "png"};
+            if (ModelState.IsValid)
+            {
+                var url = "";
+                if (foto != null && foto.ContentLength > 0 && validos.Contains(
+                    foto.FileName.Substring(foto.FileName.LastIndexOf(".")+1)
+                    ))
+                {
+                    var fichero = DateTime.Now.Ticks + "." +
+                        foto.FileName.Substring(foto.FileName.LastIndexOf(".") + 1);
+                    url = Server.MapPath("~/fotos");
+                    url += "/" + fichero;
+                    foto.SaveAs(url);
+                    productos.foto = "/fotos/" + fichero;
+                }
+
+
+                db.Productos.Add(productos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(productos);
+        }
+
+        // GET: Productos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productos productos = db.Productos.Find(id);
+            if (productos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productos);
+        }
+
+        // POST: Productos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,precio,foto")] Productos productos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productos);
+        }
+
+        // GET: Productos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Productos productos = db.Productos.Find(id);
+            if (productos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productos);
+        }
+
+        // POST: Productos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Productos productos = db.Productos.Find(id);
+            db.Productos.Remove(productos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
